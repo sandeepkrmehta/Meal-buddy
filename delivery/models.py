7dd373f7ac46a4a70fb967cd7e53ddd4dee32d5c
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-05-14 19:38:10 +0530

    Third commit

--- delivery/models.py
+++ delivery/models.py
@@ -14,4 +14,18 @@ class Restaurant(models.Model):
     cuisine = models.CharField(max_length = 200)
     rating = models.FloatField()
     
-  
+class Item(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE, related_name = "items")
+    name = models.CharField(max_length = 20)
+    description = models.CharField(max_length = 200)
+    price = models.FloatField()
+    vegeterian = models.BooleanField(default=False)
+    picture = models.URLField(max_length = 400, default='https://www.indiafilings.com/learn/wp-content/uploads/2024/08/How-to-Start-Food-Business.jpg')
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE, related_name = "cart")
+    items = models.ManyToManyField("Item", related_name = "carts")
+
+    def total_price(self):
+        return sum(item.price for item in self.items.all())
+    
